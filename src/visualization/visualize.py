64f592ffda2ce9a490c145faa1ac71475390a72a
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-01-24 20:23:41 +0700

    add function to plot train and test data

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,7 +5,7 @@ import seaborn as sns
 def yearly_seasonality_plot(
         data: pd.DataFrame, 
         date_column: str, 
-        target: str, 
+        target_column: str, 
         title: str
     ) -> None:
     """
@@ -14,7 +14,7 @@ def yearly_seasonality_plot(
     Args:
         data (pd.DataFrame) : Actual Values.
         date_column (str)   : Data Column Name.         
-        target (str)        : Target Column Name.
+        target_column (str) : Target Column Name.
         title (str)         : Title for The Plot
     
     Returns:
@@ -26,13 +26,13 @@ def yearly_seasonality_plot(
     ax = sns.lineplot(
         data=data,
         x='month',
-        y=target,
+        y=target_column,
         hue='year',
     )
     ax.set(
         title=title,
         xlabel='month',
-        ylabel=target
+        ylabel=target_column
     )
     plt.show()
     
@@ -40,7 +40,7 @@ def yearly_seasonality_plot(
 def subseries_yearly_seasonality_plot(
         data: pd.DataFrame, 
         date_column: str, 
-        target: str, 
+        target_column: str, 
         title: str
     ) -> None:
     """
@@ -50,7 +50,7 @@ def subseries_yearly_seasonality_plot(
     Args:
         data (pd.DataFrame) : Actual Values.
         date_column (str)   : Data Column Name.         
-        target (str)        : Target Column Name.
+        target_column (str) : Target Column Name.
         title (str)         : Title for The Plot
     
     Returns:
@@ -58,18 +58,48 @@ def subseries_yearly_seasonality_plot(
     """
     data.loc[:, 'month'] = data[date_column].dt.month
     data.loc[:, 'year'] = data[date_column].dt.year
-    mean_by_month = data.groupby(by='month').agg({target: 'mean'})
+    mean_by_month = data.groupby(by='month').agg({target_column: 'mean'})
     data = data.merge(mean_by_month, on='month', how='left')
     fg = sns.relplot(
         data=data,
         x='year',
-        y=target+'_x',
+        y=target_column+'_x',
         col='month',
         kind='line'
     )
 
     for i, ax in enumerate(fg.axes.flat):
-        sns.lineplot(data=data[data['month']==i+1], x='year', y=target+'_y', color='orange', ax=ax)
+        sns.lineplot(data=data[data['month']==i+1], x='year', y=target_column+'_y', color='orange', ax=ax)
     
     fg.figure.suptitle(title)
+    plt.show()
+    
+    
+def train_test_plot(
+        train: pd.DataFrame, 
+        test: pd.DataFrame, 
+        date_column: str, 
+        target_column: str, 
+        title: str
+    ) -> None:
+    """
+    Create Multiple Line Plots Separated by Month, and Inside Each Plot Contain The
+    Values from The Same Month for Different Year.
+    
+    Args:
+        data (pd.DataFrame) : Actual Values.
+        date_column (str)   : Data Column Name.         
+        target_column (str) : Target Column Name.
+        title (str)         : Title for The Plot
+    
+    Returns:
+        None
+    """
+    plt.figure(figsize=(10, 5))
+    plt.plot(train[date_column], train[target_column], label='Train')
+    plt.plot(test[date_column], test[target_column], label='Test')
+    plt.title(title, size=20)
+    plt.xlabel("Date", size=15)
+    plt.ylabel(target_column, size=15)
+    plt.legend()
     plt.show()
